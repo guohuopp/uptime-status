8ff392c98f6456df199754ebe35a6032f28a4b14
guohuopp <182708606+guohuopp@users.noreply.github.com>
2026-05-28 11:12:23 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -6,8 +6,8 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
+    'ur2895179-3f53714943878fe8f96aabc2',
+    'ur3267589-8f0b05c0ab405dece1d597f8',
     'm784497419-de55aa09902ccb3ab22d548a',
     'm784496436-71a4bf7b1e3bdf7756be131b',
   ],
